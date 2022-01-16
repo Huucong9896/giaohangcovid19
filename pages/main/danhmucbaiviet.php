d70868f56396f50a4fa919b3fb447aeed97c3098
Huucong9896 <congpt0989842523@gmail.com>
2022-01-16 19:04:20 +0700

    35 bootstrap

--- pages/main/danhmucbaiviet.php
+++ pages/main/danhmucbaiviet.php
@@ -7,21 +7,23 @@
     $row_title =mysqli_fetch_array($query_lay);
 ?>
 
-<h3>Tin : <span style="text-align: center; text-transform: uppercase; font-style: italic;color: darkgreen;"><?php echo $row_title['tendanhmuc_baiviet'] ?></span> </h3>
-                <ul class="product_list">
+<h3>Tin : <span style="text-align: center; text-transform: uppercase; font-style: italic;color: darkgreen;"><?php echo $row_title['tendanhmuc_baiviet'] ?></span></h3>
+                
+    <ul class="product_list">
+    <!-- <div class="row"> -->
                     <?php 
                         while($row_bv = mysqli_fetch_array($query_bv)){
                     ?>
-                    <li>
+                    <div class="col-md-3">
                         <a href="index.php?quanly=baiviet&id=<?php echo $row_bv['id'] ?>">
-                        <img src="admincp/modules/quanlybaiviet/uploads/<?php echo $row_bv['hinhanh']?>">
+                        <img class="img img-responsive" width="100%" src="admincp/modules/quanlybaiviet/uploads/<?php echo $row_bv['hinhanh']?>">
                             <p class="tittle_product">Tên Bài Viết : <?php echo $row_bv['tenbaiviet']?></p>
                             
                         </a>
                         <p class="tittle_product"><?php echo $row_bv['tomtat']?></p>
-                    </li>
+                    </div>
                    <?php
-                        } 
-                   ?>
-                    
-                </ul>
+                    } 
+                   ?> 
+    <!-- </div> -->
+    </ul>
--- pages/menu.php
+++ pages/menu.php
@@ -9,6 +9,66 @@
     }
 
 ?>
+<!-- new menu -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="width: 100%;">
+  <a class="navbar-brand" href="index.php">YORI img</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Trang chủ <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?quanly=giohang">Giỏ hàng</a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Danh Mục Thực Phẩm
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <?php 
+            while($row_danhmuc=mysqli_fetch_array($query_danhmuc)){
+        ?>
+        <a class="dropdown-item" href="index.php?quanly=danhmucsanpham&id=<?php echo $row_danhmuc['id_danhmuc'] ?>">
+        <?php echo $row_danhmuc['tendanhmuc'] ?></a>
+        <?php 
+        }
+        ?> 
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?quanly=tintuc">Tin Tức</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?quanly=lienhe">Tuyển Dụng</a>
+      </li>
+      <li class="nav-item">
+         <?php 
+            if(isset($_SESSION['dangky'])){
+        ?>
+            <li class="nav-item"><a class="nav-link" href="index.php?dangxuat=1">Đăng Xuất</a></li>
+            <li class="nav-item"><a class="nav-link" href="index.php?quanly=thaydoimatkhau">Thay Đổi Mật Khẩu</a></li>
+        <?php 
+            }else{
+        ?>
+            <li class="nav-item"><a class="nav-link" href="index.php?quanly=dangky">Đăng Ký</a></li>
+        <?php 
+            }  
+        ?>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0" action="index.php?quanly=timkiem" autocomplete="off" method="POST">
+      <input class="form-control mr-sm-2" type="text" name="tukhoa" placeholder="Tên sản phẩm...">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="timkiem">Tìm Kiếm</button>
+    </form>
+  </div>
+</nav>
+
+<!-- menu cu -->
 <div class="menu">
             <ul class="listmenu">
                 <li><a href="index.php">Trang chủ</a></li>
